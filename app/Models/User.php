34a0be1f452feccedd80614898aee13310b6d83b
Jefferson Severo <jeffersonsevero08@gmail.com>
2021-06-30 23:59:23 -0300

    migrations + models

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
+
+    protected $table = 'users';
 
     /**
      * The attributes that should be hidden for arrays.
