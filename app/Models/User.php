1e099d607d8843721776d2026a4fbdfe6e13e05a
Jefferson Severo <jeffersonsevero08@gmail.com>
2021-07-04 13:33:41 -0300

    get role

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+
+    public function getRoleAttribute($value)
+    {
+        return ($value == 1 ? "Admin" : "Atendimento");
+    }
+
+
 }
