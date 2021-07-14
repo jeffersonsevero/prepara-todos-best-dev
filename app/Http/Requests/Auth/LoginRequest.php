3764189f0cae6cf0403a590a02ce4886deb6dd2e
Jefferson Severo <jeffersonsevero08@gmail.com>
2021-07-14 08:45:57 -0300

    q

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -13,7 +13,7 @@ use Illuminate\Validation\ValidationException;
 class LoginRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * Determine if the user is authorized to make this request.//
      *
      * @return bool
      */
